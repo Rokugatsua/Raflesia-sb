e2ccb623425546309e0f42a80ddd96cf78fb21a6
rokugatsua <rokugatsua@outlook.co.id>
2020-06-17 21:22:01 +0700

    add jset set() function and init ui, web

--- app/script/jnote.py
+++ app/script/jnote.py
@@ -22,6 +22,7 @@ class jset:
     info_path = setting_path
     def __init__(self, setting_file=setting_path):
         self.setting_file = setting_file
+        self._temp = {}
 
     def get(self, keys):
         "getting value with keys"
@@ -29,5 +30,24 @@ class jset:
             setting_data = json.load(jfile)
 
             for key, value in setting_data.items():
+                self._temp[key] = value
                 if key == keys:
                     return value
+
+    def set(self, keys, values, new=False):
+        "insert or update value in setting file with criteria"
+        if not self._temp:
+            self.get(keys)
+        with open(self.setting_file, 'w') as jfile:
+            if not new:
+                for key in self._temp.keys():
+                    if key == keys:
+                        self._temp[key] = values
+            else:
+                self._temp[keys] = values
+
+            if self._temp:
+                json.dump(obj=self._temp, fp=jfile, indent=4)
+
+
+database_path = jpath(jset().get('database'))
