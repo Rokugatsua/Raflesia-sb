716017bcbd64579df8fc9502890d9c4723410e0a
rokugatsua <rokugatsua@outlook.co.id>
2020-06-17 19:52:14 +0700

    add jset for read setting file and get method for retrun value with criteria

--- app/script/jnote.py
+++ app/script/jnote.py
@@ -4,7 +4,6 @@ import os
 
 
 # Default JPath or Json Path
-
 def jpath(json_file:str='default.json') -> str:
     " Return path of data file json "
     curdir = os.getcwd()
@@ -13,3 +12,22 @@ def jpath(json_file:str='default.json') -> str:
         return os.path.join("".join(scurdir[-1]), 'data', json_file)
     else:
         return os.path.join(curdir, 'app', 'data', json_file)
+
+
+# __setting path__
+
+setting_path = jpath('setting.json')
+
+class jset:
+    info_path = setting_path
+    def __init__(self, setting_file=setting_path):
+        self.setting_file = setting_file
+
+    def get(self, keys):
+        "getting value with keys"
+        with open(self.setting_file, 'r') as jfile:
+            setting_data = json.load(jfile)
+
+            for key, value in setting_data.items():
+                if key == keys:
+                    return value
