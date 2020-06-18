ebcc0f2b33054c977341a61e5bd58e336458ef34
rokugatsua <rokugatsua@outlook.co.id>
2020-06-18 19:42:22 +0700

    add model.Acount

--- app/script/model.py
+++ app/script/model.py
@@ -20,7 +20,10 @@ class Model(object):
     @property
     def value(self):
         f"get value from '{self.key}' key"
-        return self.__temp[self.key]
+        try:
+            return self.__temp[self.key]
+        except KeyError as e:
+            print(f"'{e}' not found on database" )
 
     def commit(self):
         self.notation.set(value=self.value, key=self.key)
@@ -28,7 +31,79 @@ class Model(object):
 
 
 class Account(Model):
-    pass
+    def __init__(self, val_type):
+        super().__init__()
+        self.val_type = val_type
+
+    def new_init(self):
+        super().new_init(self.val_type)
+
+    def add_category(self, key:str):
+        key.lower()
+        if key not in self.value:
+            self.value[key] = {}
+        else:
+            print(f"'{key}' already exist")
+
+    def delete_category(self, key:str):
+        key.lower()
+        if key in self.value:
+            del self.value[key]
+        else:
+            print(f"'{key}' not found")
+
+    def edit_category(self, old_key:str, new_key:str):
+        old_key.lower()
+        new_key.lower()
+        val = {}
+        if old_key in self.value and new_key not in self.value:
+            val = self.value[old_key]
+            self.value[new_key] = val
+
+            del self.value[old_key]
+
+    def add_account(self, key:str, value:int, category:str):
+        key.lower()
+        try:
+            if key not in self.value[category.lower()]:
+                self.value[category][key] = value
+            else:
+                print(f"'{key}' already exist")
+        except KeyError as e:
+            print(f"{e} not found")
+
+    def delete_account(self, key:str, category:str):
+        key.lower()
+        try:
+            if key in self.value[category.lower]:
+                del self.value[category][key]
+            else:
+                print(f"'{key}' not found")
+        except KeyError as e:
+            print(f"{e} not found")                
+
+    def edit_account(self, old_key:str, new_key:str, category:str):
+        old_key.lower()
+        new_key.lower()
+        val = {}
+        try:
+            if old_key in self.value[category.lower()] \
+                and new_key not in self.value[category.lower()]:
+                val = self.value[category][old_key]
+                self.value[category][new_key] = val
+
+                del self.value[old_key]
+        except KeyError as e:
+            print(f"{e} not found")
+
+    def update_account(self, key:str, value:int, category:str):
+        key.lower()
+        try:
+            if key in self.value[category.lower()]:
+                self.value[category][key] =  value
+        except KeyError as e:
+            print(f"{e} not found")
+        
 
 class Income(Model):
     def add(self, value):
@@ -63,3 +138,13 @@ class Income(Model):
             index = self.value.index(old_value)
             self.value[index] = new_value
 
+class Expense(Income):
+    pass
+
+class Budget(Model):
+    def __init__(self, val_type):
+        super().__init__()
+        self.val_type = val_type
+    
+    def new_init(self):
+        super().new_init(self.val_type)
