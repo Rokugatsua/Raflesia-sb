1604bf3ef2f81c1b8c54ef48da9676f4c89e1878
rokugatsua <rokugatsua@outlook.co.id>
2020-06-18 11:42:03 +0700

    fix models

--- app/script/model.py
+++ app/script/model.py
@@ -4,9 +4,10 @@ class Model(object):
     def __init__(self, notation=jnote.Notation):
         self.key = str(type(self).__name__).lower()
         self.notation = notation(self.key)
+        self.__temp = self.notation.get(self.key)
 
     def items(self) -> dict:
-        return self.notation.get(self.key)
+        return self.__temp
 
     def new_init(self, val_type=dict()):
         if not self.items():
@@ -14,8 +15,12 @@ class Model(object):
         else:
             print(self.key, 'already exists')
 
+    @property
     def value(self):
-        return self.items()[self.key]
+        f"get value from '{self.key}' key"
+        return self.__temp[self.key]
+
+
 
 
 class Account(Model):
